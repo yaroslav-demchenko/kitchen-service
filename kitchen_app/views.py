af194d54f64fb42e290cd0c894d72cf6c90b1708
Yaroslav Demchenko <dastex1411@gmail.com>
2022-12-22 16:07:30 +0200

    Added success_url to CookCreateView in views.py

--- kitchen_app/views.py
+++ kitchen_app/views.py
@@ -119,6 +119,7 @@ class CooksDetailView(LoginRequiredMixin, generic.DetailView):
 class CookCreateView(LoginRequiredMixin, generic.CreateView):
     model = Cook
     form_class = CookCreationForm
+    success_url = reverse_lazy("kitchen_app:cook_list")
 
 
 class CookUpdateView(LoginRequiredMixin, generic.UpdateView):
